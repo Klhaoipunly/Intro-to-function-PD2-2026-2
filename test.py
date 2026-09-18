b7acb78910ba244033fcd2796376fd7be7350537
Klhaoipunly <junze1212jl@icloud.com>
2026-09-18 09:20:31 -0400

    asdasd

---
+++ test.py
@@ -0,0 +1,21 @@
+#string for characters
+name = "Inaya"
+print(name.upper())
+#input asks the uses a question and records the answer
+#what we write in input argument is what user sees input ALWAYS outputs a string
+#bill = input("How much was the bill")
+#print(bill)
+
+#if bill == 10:
+#    print("match")
+#else:
+#    print("no match")
+
+#""" #integer for whole number
+#amt = 100
+#Float uses decimal
+#amt_two = 99.99 """
+
+#Boolean
+#x= True
+#y= False
