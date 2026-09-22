17928e9559fcc420f92315a9c4ca4dd58f57d95c
Klhaoipunly <junze1212jl@icloud.com>
2026-09-22 06:44:50 -0400

    9/22/2026 6:44

---
+++ data.py
@@ -0,0 +1,55 @@
+#def odd_or_even(number):
+#    if number % 2 == 0:
+#        return "even"
+#    else:
+#        return "odd"
+
+#number = int(input("Enter a number: "))
+#print(odd_or_even(number))
+
+
+
+
+
+#def tipcalculator(bill, service):
+#    if service == "bad":
+#        tip_percent = 0
+#    elif service == "okay":
+#        tip_percent = 15
+#    elif service == "good":
+#        tip_percent = 20
+#    elif service == "great":
+#        tip_percent = 25
+#    else:
+#        return "Invalid service rating"
+
+#    tip = bill * tip_percent / 100
+#    total = bill + tip
+
+#    return total
+
+#bill = float(input("Enter the bill: "))
+
+#service = input("How was the service? ")
+
+#total = tipcalculator(bill, service)
+#print(f"Total: ${total:.2f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
