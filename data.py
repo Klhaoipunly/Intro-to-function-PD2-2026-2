f2394ccb3b1d481d645b87e9be86ee720d8bf0b9
Klhaoipunly <junze1212jl@icloud.com>
2026-09-23 09:25:48 -0400

    9/23/2026 9:25

--- data.py
+++ data.py
@@ -10,12 +10,33 @@
 
 
 
-person = input("Please type a persons name")
-object = input("Please type a object")
-verb = input("Please type a verb")
-verb2 = input("Please type a different verb")
+#FamousPerson = input("Please type a famous persons name: ")
+#Place = input("Please type a place: ")
+#PluralNoun = input("Please type a plural noun: ")
+#Adj = input("Please type an adjective: ")
+#Obj = input("Please type an object: ")
+#Verbendingining = input("Please type a verb ending in -ing: ")
+#WeatherCondition = input("Please type a weather condition: ")
+#Pronoun = input("Please type a pronoun: ")
+#FamousPerson2 = input("Please type another famous persons name: ")
+#Exclamation = input("Please type an exclamation (like WOW or AHH): ")
+#Verb2 = input("Please type another verb: ")
+#print (f"One day, {FamousPerson} decided to visit {Place}. They packed a bag full of {PluralNoun} and a very {Adj} {Obj}. As soon as they arrived, they started {Verbendingining} because the weather was so {WeatherCondition}. Suddenly, a {Pronoun} {FamousPerson2} jumped out and shouted, {Exclamation}! This made everyone {Verb2} all the way home.")
 
 
 
+#def odd_or_even(number):
+#    if number % 2 == 0:
+#        return "even"
+#    else:
+#        return "odd"
 
+
+#number = int(input("Enter a number: "))
+#print(odd_or_even(number))
+
+
+
+def tip_calc():
+    
 
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #string for characters
-name = "Inaya"
-print(name.upper())
+#name = "Inaya"
+#print(name.upper())
 #input asks the uses a question and records the answer
 #what we write in input argument is what user sees input ALWAYS outputs a string
 #bill = input("How much was the bill")
@@ -19,3 +19,31 @@ print(name.upper())
 #Boolean
 #x= True
 #y= False
+
+
+
+
+
+
+
+
+#integer
+x = 7
+#String
+name= "Ellie"
+#name.upper
+#boolean
+isValid= True
+#Float
+bill = 56.86
+
+
+students = ["Ellie", "Preston", "Ben", "Elyse"]
+students.append("Sofia")
+print (students [-1])
+for student in students:
+    if student == "Ben":
+        print(f'we found {student}')
+#sting
+y = input("money?")
+z = y + 5
