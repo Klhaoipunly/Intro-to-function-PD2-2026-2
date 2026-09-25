69c9c483542ddfc72ff571acc2f752425d3f45b4
Klhaoipunly <junze1212jl@icloud.com>
2026-09-25 09:24:23 -0400

    9/25/2026 9:24

--- data.py
+++ data.py
@@ -10,18 +10,18 @@
 
 
 
-FamousPerson = input("Please type a famous persons name: ")
-Place = input("Please type a place: ")
-PluralNoun = input("Please type a plural noun: ")
-Adj = input("Please type an adjective: ")
-Obj = input("Please type an object: ")
-Verbendingining = input("Please type a verb ending in -ing: ")
-WeatherCondition = input("Please type a weather condition: ")
-sexori = input("Please type a sexual orientation: ")
-FamousPerson2 = input("Please type another famous persons name: ")
-Exclamation = input("Please type an exclamation (like WOW or AHH): ")
-Verb2 = input("Please type another verb: ")
-print (f"One day, {FamousPerson} decided to visit {Place}. They packed a bag full of {PluralNoun} and a very {Adj} {Obj}. As soon as they arrived, they started {Verbendingining} because the weather was so {WeatherCondition}. Suddenly, a {sexori} {FamousPerson2} jumped out and shouted, {Exclamation}! This made everyone {Verb2} all the way home.")
+#FamousPerson = input("Please type a famous persons name: ")
+#Place = input("Please type a place: ")
+#PluralNoun = input("Please type a plural noun: ")
+#Adj = input("Please type an adjective: ")
+#Obj = input("Please type an object: ")
+#Verbendingining = input("Please type a verb ending in -ing: ")
+#WeatherCondition = input("Please type a weather condition: ")
+#sexori = input("Please type a sexual orientation: ")
+#FamousPerson2 = input("Please type another famous persons name: ")
+#Exclamation = input("Please type an exclamation (like WOW or AHH): ")
+#Verb2 = input("Please type another verb: ")
+#print (f"One day, {FamousPerson} decided to visit {Place}. They packed a bag full of {PluralNoun} and a very {Adj} {Obj}. As soon as they arrived, they started {Verbendingining} because the weather was so {WeatherCondition}. Suddenly, a {sexori} {FamousPerson2} jumped out and shouted, {Exclamation}! This made everyone {Verb2} all the way home.")
 
 
 
@@ -31,26 +31,44 @@ print (f"One day, {FamousPerson} decided to visit {Place}. They packed a bag ful
 #    else:
 #        return "odd"
 
-
 #number = int(input("Enter a number: "))
 #print(odd_or_even(number))
 
 
 
+#def tipcalc(bill, service):
+#   if service == "bad":
+#       tip_percent = 0
+#   elif service == "okay":
+#       tip_percent = 15
+#   elif service == "good":
+#       tip_percent = 20
+#   elif service == "great":
+#       tip_percent = 25
+#   elif service == "67":
+#       tip_percent = 67
 
-#def tip_calc(bill , service):
-#    if service == "bad":
-#        tip = 0
-#    elif service == "okay":
-#        tip = 15
-#    elif service == "good":
-#        tip = 20
-#    elif service == "great":
-#        tip = 25
-#    else:
-#        tip = 12394326781984726183746
-#        print("I hate u")
+#   tip = bill * tip_percent / 100
+#   total = bill + tip
+
+#   return total
+
+#bill = float(input("Enter the your bill: "))
+
+#service = input("How was the service?:")
+
+#total = tipcalculator(bill, service)
+#print(f"Total: ${total:.2f}")
 
 
-#bill = input("Please enter your bill: ")
-#service = input("Please enter how well your experience is")
+number = float(input("Enter a number: "))
+
+def factor():
+    for i range()
+        
+
+
+
+print(factor)
+
+
